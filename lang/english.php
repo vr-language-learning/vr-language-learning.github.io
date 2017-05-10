f0f8036a3fb8ef9ef4daebb913494844d5ab442f
CoralieGold <coralie.gold@gmail.com>
2017-05-10 12:08:15 +0200

    Init

---
+++ lang/english.php
@@ -0,0 +1,38 @@
+<?php 
+
+// MENU
+$home = "Home";
+$about = "About";
+$screenshots = "Screenshots";
+$download = "Download";
+$team = "Team";
+$contact = "Contact";
+
+
+// HOME
+
+
+// ABOUT
+$about_title = "About";
+$about_text = "VR Language Learning is a serious game in VR created to learn languages. It is a project realized by students of the <a href='http://www.ingenieur-imac.fr/' target='_blank'>IMAC</a>'s enginering school.";
+
+// SCREENSHOTS
+$screenshots_title = "Screenshots";
+
+// DOWNLOAD
+$download_title = "Download";
+$download_install = "How to install the application?";
+$download_1 = 'Download the application (56 Mo) with the button "<i class="fa fa-download fa-lg"></i>".';
+$download_2 = "Put the file downloaded (VRLanguageLearning.apk) on your smartphone.";
+$download_3 = 'With your smartphone, select the file and click on "install".';
+$download_4 = "Don't forget to activate your microphone and to increase the \"multimedia\" (or \"media\") volume, on your phone's parameters.";
+$download_important = "<b>IMPORTANT:</b> The application works only on Android smartphones which can run VR apps!";
+
+// TEAM
+$team_title = "team";
+$team_dev = "Developer";
+$team_3D = "3D Graphic Designer";
+$team_manager = "Project Manager & Developer";
+
+
+?>
